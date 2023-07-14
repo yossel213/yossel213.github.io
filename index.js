7f8c7ec238dde123f0201aeb01e725c0e72feea0
yossel213 <119770938+yossel213@users.noreply.github.com>
2023-07-14 15:11:01 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $("#btnLogin").click(function(){
+        let usuario = $("#username").val();
+        let password = $("#password").val();
+        localStorage.setItem("usuario", usuario);
+        localStorage.setItem("password", password);
+        window.location.href = "pages/home.html";
+    })
+
+    $("#showPass").click(function() {
+        var passwordInput = $("#password");
+
+        if (passwordInput.attr("type") === "password") {
+          passwordInput.attr("type", "text");
+        } else {
+          passwordInput.attr("type", "password");
+        }
+      });
+  });
