12808efe1f9aa30fdd5fde9f4273c7052e31a201
yossel213 <119770938+yossel213@users.noreply.github.com>
2023-07-14 21:06:19 -0500

    Update index.js

--- index.js
+++ index.js
@@ -17,3 +17,11 @@ $(document).ready(function() {
         }
       });
   });
+
+$.ajax({
+    url: 'https://randomuser.me/api/',
+    dataType: 'json',
+    success: function(data) {
+      console.log(data);
+    }
+});
